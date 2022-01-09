9527b565d4527571e3aef215f8060428197ecacb
AditiRokade <aditi.rokade.ajr08@gmail.com>
2022-01-10 00:18:04 +0530

    Q69

---
+++ Easy/day2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class day2 {
+    public static void main(String[] args){
+        
+        System.out.println("Enter a number:");
+        Scanner in = new Scanner(System.in);
+        int x=in.nextInt();
+        
+        System.out.println(mySqrt(x));
+    }
+    public static int mySqrt(int x) {
+        if (x == 0 || x == 1)   
+            return x;
+
+        int start = 1, end = x/2, sqrt = 0;  
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == x / mid)     
+                return mid;
+
+            else if (mid < x/mid)  
+            {
+                sqrt = mid;         //either the middle element is the possible answer, or
+                start = mid + 1;    //the answer lies in the second half
+            }
+            else
+                end = mid - 1;     
+        }
+        return sqrt;
+    }
+        
+    }
+
